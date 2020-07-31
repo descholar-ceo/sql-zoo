84808ddcf728ff1cdafa1d2abaae437bc901323d
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 01:22:42 +0200

    display countrie between a certain areabetween keyword

--- 00-select-basics.sql
+++ 00-select-basics.sql
@@ -5,3 +5,7 @@ SELECT population FROM world
 -- display population from sweden, norway, and denmark using IN keyword
 SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- displaying list of countries which are between  a certain area, using BETWEEN keyword
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
