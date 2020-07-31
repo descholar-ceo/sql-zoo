2d74577fbc8df36209557978a7e521ce2eab2063
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 01:20:49 +0200

    displaying population from sweden, norway, denmark using in keyword

--- 00-select-basics.sql
+++ 00-select-basics.sql
@@ -1,3 +1,7 @@
--- displaying population from Germany
+-- display population from Germany using WHERE keyword
 SELECT population FROM world
   WHERE name = 'Germany'
+
+-- display population from sweden, norway, and denmark using IN keyword
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
