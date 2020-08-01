31322c65b5c99cdef9460a2e5a91d89d35dbd42c
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 23:23:30 +0200

    Add answer 3

--- 10-join.sql
+++ 10-join.sql
@@ -3,3 +3,9 @@
 -- Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
 SELECT matchid, player FROM goal 
   WHERE teamid = 'GER'
+
+-- QUESTION 2. Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+  FROM game WHERE id=1012
+
+  -- QUESTION 3. 
