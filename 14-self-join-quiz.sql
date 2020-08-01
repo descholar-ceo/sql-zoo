f50de0fd5a56d12aeca7b02ae4edb40ed9d16e01
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-02 01:59:21 +0200

    Add last quiz answer n2

--- 14-self-join-quiz.sql
+++ 14-self-join-quiz.sql
@@ -4,3 +4,12 @@ SELECT DISTINCT a.name, b.name
   JOIN route y ON y.num = z.num
   JOIN stops b ON y.stop=b.id
  WHERE a.name='Craiglockhart' AND b.name ='Haymarket'
+
+ -- QUESTION 2. Select the code that shows the stops that are on route.num '2A' which can be reached with one bus from Haymarket?
+SELECT S2.id, S2.name, R2.company, R2.num
+  FROM stops S1, stops S2, route R1, route R2
+ WHERE S1.name='Haymarket' AND S1.id=R1.stop
+   AND R1.company=R2.company AND R1.num=R2.num
+   AND R2.stop=S2.id AND R2.num='2A'
+
+
