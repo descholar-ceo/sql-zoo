02108e585476bb312013dcdffcfe28d55c3a92a8
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 17:21:30 +0200

    Add three last answers

--- 05-select-from-nobel-quiz.sql
+++ 05-select-from-nobel-quiz.sql
@@ -19,4 +19,31 @@ Medicine	Sir John Eccles
 Medicine	Sir Frank Macfarlane Burnet
 '
 
+-- QUESTION 5. Select the code which would show the year when neither a Physics or Chemistry award was given
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
 
+-- QUESTION 6. Select the code which shows the years when a Medicine award was given but no Peace or Literature award was
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+-- QUESTION 7. Pick the result that would be obtained from the following code:
+SELECT subject, COUNT(subject) 
+   FROM nobel 
+  WHERE yr ='1960' 
+  GROUP BY subject
+
+'
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	    1
+Physics	    1
+'
