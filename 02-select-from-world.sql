9ba500e67b6e24ce933bfe4c2c2b2f2ae246681f
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 11:22:45 +0200

    solve first 3 questions

---
+++ 02-select-from-world.sql
@@ -0,0 +1,12 @@
+-- QUESTION 1
+SELECT name, continent, population FROM world
+
+-- QUESTION 2
+SELECT name FROM world
+WHERE population >= 200000000
+
+-- QUESTION 3
+SELECT name, gdp/population FROM world WHERE population >= 200000000
+
+-- QUESTION 4
+
