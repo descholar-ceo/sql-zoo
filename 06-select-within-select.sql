41b275e37795336ecc9fc86aa4cd1831a916753c
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 18:17:31 +0200

    Add two q

--- 06-select-within-select.sql
+++ 06-select-within-select.sql
@@ -23,3 +23,35 @@ SELECT name, population
 FROM world
 WHERE population>(SELECT population FROM world WHERE name='Canada') 
 AND population < (SELECT population FROM world WHERE name='Poland')
+
+
+'
+QUETSION 5.
+
+Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+The format should be Name, Percentage for example:
+
+name	percentage
+Albania	3%
+Andorra	0%
+Austria	11%
+...	...
+
+NOTE: Decimal places
+You can use the function ROUND to remove the decimal places.
+Percent symbol %
+You can use the function CONCAT to add the percentage symbol.
+
+'
+
+SELECT name, CONCAT(ROUND((population*100)/(SELECT population FROM world WHERE name='Germany'),0),'%') 
+FROM world WHERE continent='Europe'
+
+-- Question 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name
+FROM world
+WHERE gdp > (SELECT MAX(gdp) FROM world WHERE continent='Europe')
+
