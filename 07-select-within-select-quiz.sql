5a75fd8cc7c31158fcdb1af7884d003ca0f4acca
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 21:10:37 +0200

    Add answer 3

--- 07-select-within-select-quiz.sql
+++ 07-select-within-select-quiz.sql
@@ -5,3 +5,7 @@ WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AN
 -- QUESTION 2. Select the code that shows the countries belonging to regions with all populations over 50000
 SELECT name,region,population FROM bbc x 
 WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+
+-- QUESTION 3. Select the code that shows the countries with a less than a third of the population of the countries around it
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
