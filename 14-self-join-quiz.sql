9fe2a6e1576fe70bb3e332cda3489ce7675cad0e
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-02 01:58:18 +0200

    Add last quiz answer n1

---
+++ 14-self-join-quiz.sql
@@ -0,0 +1,6 @@
+-- QUESTION 1. Select the code that would show it is possible to get from Craiglockhart to Haymarket
+SELECT DISTINCT a.name, b.name
+  FROM stops a JOIN route z ON a.id=z.stop
+  JOIN route y ON y.num = z.num
+  JOIN stops b ON y.stop=b.id
+ WHERE a.name='Craiglockhart' AND b.name ='Haymarket'
