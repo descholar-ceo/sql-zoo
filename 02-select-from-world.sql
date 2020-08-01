43cec07e7f5868329b9bb0596d22f2d03d2497af
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 12:16:05 +0200

    Solve the next three questions

--- 02-select-from-world.sql
+++ 02-select-from-world.sql
@@ -20,3 +20,10 @@ SELECT name from world WHERE name like 'United%'
 -- QUESTION 7
 SELECT name, population, area FROM world WHERE area>=3000000 OR population >= 250000000
 
+-- QUESTION 8
+SELECT name, population, area FROM world WHERE area >= 3000000 XOR population >= 250000000
+
+-- QUESTION 9
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2) FROM world WHERE continent='South America'
+
+
