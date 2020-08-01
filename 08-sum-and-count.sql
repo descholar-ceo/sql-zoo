4f0eb35c61089b0dd8ef172d877af6e8a9bc8f85
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 21:35:53 +0200

    Add answer 3

--- 08-sum-and-count.sql
+++ 08-sum-and-count.sql
@@ -4,3 +4,8 @@ FROM world
 
 -- QUESTION 2. List all the continents - just once each.
 SELECT DISTINCT(continent) FROM world
+
+-- QUESTION 3. Give the total GDP of Africa
+SELECT SUM(gdp) FROM world WHERE continent='Africa'
+
+
