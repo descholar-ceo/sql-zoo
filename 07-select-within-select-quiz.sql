c388bd9de162649663ec2579d2176e342f54e8bd
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-01 21:09:26 +0200

    Add answer 2

--- 07-select-within-select-quiz.sql
+++ 07-select-within-select-quiz.sql
@@ -1,3 +1,7 @@
 -- QUESTION 1. Select the code that shows the name, region and population of the smallest country in each region
 SELECT region, name, population FROM bbc x 
 WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
+
+-- QUESTION 2. Select the code that shows the countries belonging to regions with all populations over 50000
+SELECT name,region,population FROM bbc x 
+WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
